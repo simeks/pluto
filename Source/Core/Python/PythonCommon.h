116b7c5a14de58493a921bdebb2f22f8caec89d9
Simon Ekström <simon.ekstrom@surgsci.uu.se>
2016-12-30 02:50:02 +0100

    Added macros for printing to python stdout and stderr.

--- Source/Core/Python/PythonCommon.h
+++ Source/Core/Python/PythonCommon.h
@@ -8,5 +8,7 @@
 #define PYTHON_ERROR(Exc, Text, ...) do{PyErr_Format(PyExc_##Exc, Text, __VA_ARGS__); return;}while(0)
 #define PYTHON_ERROR_R(Exc, Return, Text, ...) do{PyErr_Format(PyExc_##Exc, Text, __VA_ARGS__); return Return;}while(0)
 
+#define PYTHON_STDOUT(Text, ...) PySys_FormatStdout(Text, __VA_ARGS__)
+#define PYTHON_STDERR(Text, ...) PySys_FormatStderr(Text, __VA_ARGS__)
 
 #endif // __PYTHON_COMMON_H__
