345234d96342c0eeabdfd718212830a954dc503e
Simon Ekström <simon.ekstrom@surgsci.uu.se>
2017-02-08 18:38:19 +0100

    Added a node for building lists.

--- Python/flow/util.py
+++ Python/flow/util.py
@@ -44,5 +44,37 @@ class FileBrowse(FlowNode):
         ctx.write_pin('Out', self.file)
 
 
+
+@pluto_class
+class BuildList(FlowNode):
+    pins = [
+        FlowPin('Out', FlowPin.Out),
+        FlowPin('0', FlowPin.In),
+        FlowPin('1', FlowPin.In),
+        FlowPin('2', FlowPin.In),
+        FlowPin('3', FlowPin.In),
+        FlowPin('4', FlowPin.In),
+        FlowPin('5', FlowPin.In),
+        FlowPin('6', FlowPin.In),
+        FlowPin('7', FlowPin.In),
+        FlowPin('8', FlowPin.In),
+        FlowPin('9', FlowPin.In)
+    ]
+
+    def __init__(self):
+        super(BuildList, self).__init__()
+        self.node_class = 'flow.util.BuildList'
+        self.title = 'BuildList'
+        self.category = 'Flow/Util'
+
+    def run(self, ctx):
+        lst = []
+        for i in range(0, 10):
+            if self.is_pin_linked(str(i)):
+                lst.append(ctx.read_pin(str(i)))
+        ctx.write_pin('Out', lst)
+
+
 install_node_template(FileBrowse())
 install_node_template(EvalNode())
+install_node_template(BuildList())
