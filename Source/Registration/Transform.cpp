5bcc03d6a8256137776b983c11f57f40091a83d7
Simon Ekström <simon.ekstrom@surgsci.uu.se>
2017-02-15 18:33:15 +0100

    You can now transform an image of different size than the deformation field

--- Source/Registration/Transform.cpp
+++ Source/Registration/Transform.cpp
@@ -10,24 +10,60 @@ namespace transform
     template<typename TImage>
     TImage transform_image(const TImage& source, const ImageVec3d& deformation)
     {
-        assert(source.size() == deformation.size());
-
         TImage result(source.ndims(), source.size());
         result.set_spacing(source.spacing());
         result.set_origin(source.origin());
         Vec3i size = result.size();
-#pragma omp parallel for
-        for (int z = 0; z < size.z; ++z)
+
+        if (source.size() == deformation.size())
         {
-            for (int y = 0; y < size.y; ++y)
+#pragma omp parallel for
+            for (int z = 0; z < size.z; ++z)
             {
-                for (int x = 0; x < size.x; ++x)
+                for (int y = 0; y < size.y; ++y)
                 {
-                    result(x, y, z) = source.linear_at(Vec3d(x, y, z) + deformation(x, y, z));
+                    for (int x = 0; x < size.x; ++x)
+                    {
+                        result(x, y, z) = source.linear_at(Vec3d(x, y, z) + deformation(x, y, z));
+                    }
                 }
             }
+            return result;
         }
-        return result;
+        else
+        {
+            // Specialized function transforming an image of different size than vectorfield by interpolation
+
+            // TODO: Cleanup 
+            // Consider a varied range of cases: Image and vectorfield is same size but different spacing,
+            // Should world space transform be explicit?
+            
+            Vec3d img_spacing = result.spacing();
+            Vec3d def_spacing = deformation.spacing();
+
+            // TODO: 2D!
+            if (size.z == 1)
+            {
+                img_spacing.z = 1;
+                def_spacing.z = 1;
+            }
+
+
+#pragma omp parallel for
+            for (int z = 0; z < size.z; ++z)
+            {
+                for (int y = 0; y < size.y; ++y)
+                {
+                    for (int x = 0; x < size.x; ++x)
+                    {
+                        Vec3d def_p = Vec3d(x * (img_spacing.x / def_spacing.x), y * (img_spacing.y / def_spacing.y), z * (img_spacing.z / def_spacing.z));
+                        result(x, y, z) = source.linear_at(Vec3d(x, y, z) + deformation.linear_at(def_p));
+                    }
+                }
+            }
+            return result;
+
+        }
     }
 }
 
@@ -38,8 +74,8 @@ Image transform::transform(const Image& image, const Image& deformation)
         Image image_to_transform = image;
         Image result;
 
-        if (deformation.size() != image.size())
-            PYTHON_ERROR_R(ValueError, Image(), "Deformation field and image needs to be the same size");
+        //if (deformation.size() != image.size())
+        //    PYTHON_ERROR_R(ValueError, Image(), "Deformation field and image needs to be the same size");
 
         // @hack only 3d registration is supported currently so to support 2d images
         // we reshape them to 3d images with z axis set to length 1
@@ -51,11 +87,23 @@ Image transform::transform(const Image& image, const Image& deformation)
         {
             result = transform::transform_image<ImageUInt8>(image_to_transform, deformation);
         }
+        else if (image.pixel_type() == image::PixelType_Int8)
+        {
+            result = transform::transform_image<ImageInt8>(image_to_transform, deformation);
+        }
         else if (image.pixel_type() == image::PixelType_UInt16)
         {
             result = transform::transform_image<ImageUInt16>(image_to_transform, deformation);
         }
+        else if (image.pixel_type() == image::PixelType_Int16)
+        {
+            result = transform::transform_image<ImageInt16>(image_to_transform, deformation);
+        }
         else if (image.pixel_type() == image::PixelType_UInt32)
+        {
+            result = transform::transform_image<ImageUInt32>(image_to_transform, deformation);
+        }
+        else if (image.pixel_type() == image::PixelType_Int32)
         {
             result = transform::transform_image<ImageUInt32>(image_to_transform, deformation);
         }
