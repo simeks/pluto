8b24398203159c662f21f969480c84c2c0efe9d9
Simon Ekström <simon.ekstrom@surgsci.uu.se>
2017-02-15 18:31:53 +0100

    List indexing node

--- Python/flow/list.py
+++ Python/flow/list.py
@@ -39,7 +39,35 @@ class ConcatenateNode(FlowNode):
             out.extend(l)
         ctx.write_pin('Out', out)
 
+@pluto_class
+class IndexNode(FlowNode):
+    pins = [
+        FlowPin('In', FlowPin.In),
+        FlowPin('Out', FlowPin.Out)
+    ]
+    properties = [
+        StringProperty('index', '0'),
+    ]
+
+    def __init__(self):
+        super(IndexNode, self).__init__()
+        self.node_class = 'flow.list.IndexNode'
+        self.title = 'Slice'
+        self.category = 'List'
+        self.ui_class = 'one_to_one_node'
+        self.ui_node_title_var = 'index'
+
+    def run(self, ctx):
+        lst = ctx.read_pin('In')
+
+        # Validate index
+        index = self.index.strip()
+        if not re.match('[\-0-9:]+$', index):
+            raise ValueError('Invalid index')
+
+        ctx.write_pin('Out', eval('lst[%s]' % index))
 
 install_node_template(BuildNode())
 install_node_template(ConcatenateNode())
+install_node_template(IndexNode())
 
